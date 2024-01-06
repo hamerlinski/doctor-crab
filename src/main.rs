ef3b01059b41c06fc5c4c9b12cec1582151322ea
Jakub Piotr Hamerliński <115890980+hamerlinski@users.noreply.github.com>
2024-01-06 14:49:14 +0100

    refactor: format code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
-use std::fs::File;
 use aws_sdk_cloudfront as cloudfront;
 use aws_sdk_cloudfront::types::Distribution;
-use markdown_gen::markdown::{AsMarkdown, List, Markdown};
 use chrono::prelude::*;
+use markdown_gen::markdown::{AsMarkdown, List, Markdown};
+use std::fs::File;
 
-// Function to get S3 bucket information
 fn generate_distro_mk_doc(distribution: Distribution) {
     let distribution_config = distribution.distribution_config.unwrap();
     let file = File::create(format!("{}.md", distribution_config.comment).as_str()).unwrap();
     let mut md = Markdown::new(file);
     let utc: DateTime<Utc> = Utc::now();
-    md.write(format!("Cloudfront {}", distribution_config.comment).heading(1)).unwrap();
-    md.write(format!("<sub>Documentation last update: {} UTC</sub>", utc.format("%Y/%m/%d %H:%M:%S").to_string()).as_str()).unwrap();
+    md.write(format!("Cloudfront {}", distribution_config.comment).heading(1))
+        .unwrap();
+    md.write(
+        format!(
+            "<sub>Documentation last update: {} UTC</sub>",
+            utc.format("%Y/%m/%d %H:%M:%S").to_string()
+        )
+        .as_str(),
+    )
+    .unwrap();
     md.write("Parameters:".heading(2)).unwrap();
     md.write(
         List::new(false)
@@ -20,8 +27,9 @@ fn generate_distro_mk_doc(distribution: Distribution) {
             .item(format!("status: {}", distribution.status).as_str())
             .item(format!("arn: {}", distribution.arn).as_str())
             .item(format!("last_modified_time: {}", distribution.last_modified_time).as_str())
-            .item(format!("web_acl_id: {}", distribution_config.web_acl_id.unwrap()).as_str()) //TODO: add more fields
-    ).unwrap();
+            .item(format!("web_acl_id: {}", distribution_config.web_acl_id.unwrap()).as_str()), //TODO: add more fields
+    )
+    .unwrap();
 }
 
 #[tokio::main]
