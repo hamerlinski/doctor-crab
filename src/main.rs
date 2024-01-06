20a1e13c8144512e9894115136e0f1af9a5257ba
Jakub Piotr Hamerliński <115890980+hamerlinski@users.noreply.github.com>
2024-01-06 18:54:23 +0100

    feat: move distro data download to sub-module

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ mod utils;
 
 use crate::resources::distro;
 use crate::utils::search;
-use aws_sdk_cloudfront as cloudfront;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use serde_yaml;
 use std::fs::File;
 use std::io;
@@ -17,30 +16,27 @@ async fn main() -> io::Result<()> {
         File::open(path)?.read_to_string(&mut yaml_content)?;
         let config: Config = serde_yaml::from_str(&yaml_content).expect("Could not open file.");
         for (_env_name, env) in &config.environments {
-            let creds = aws_config::load_from_env().await;
-            let client = aws_sdk_cloudfront::Client::new(&creds);
-            let distro_data = client
-                .get_distribution()
-                .id(&env.resources.cloudfront)
-                .send()
-                .await
-                .unwrap();
-            let distro_output = distro_data.distribution.unwrap();
-            distro::generate_markdown(distro_output);
+            distro::generate_markdown(
+                &env.resources.cloudfront,
+                Some(format!("docs/{}/{}/", config.app, _env_name)),
+            )
+            .await;
         }
     }
 
     Ok(())
 }
+
 #[derive(Debug, Deserialize)]
 struct Resources {
     cloudfront: String,
-    // Add more resource fields as needed (e.g., cloudfront, alb, cluster)
 }
+
 #[derive(Debug, Deserialize)]
 struct Env {
     resources: Resources,
 }
+
 #[derive(Debug, Deserialize)]
 struct Config {
     app: String,
--- src/resources/distro.rs
+++ src/resources/distro.rs
@@ -1,11 +1,25 @@
 use aws_sdk_cloudfront::types::Distribution;
 use chrono::{DateTime, Utc};
 use markdown_gen::markdown::{AsMarkdown, List, Markdown};
+use std::fs;
 use std::fs::File;
 
-pub(crate) fn generate_markdown(distribution: Distribution) {
+pub(crate) async fn generate_markdown(distribution_id: &str, _output_path: Option<String>) {
+    let distribution = distribution_data(distribution_id).await;
     let distribution_config = distribution.distribution_config.unwrap();
-    let file = File::create(format!("{}.md", distribution_config.comment).as_str()).unwrap();
+    fs::create_dir_all(_output_path.clone().unwrap()).expect(&*format!(
+        "ERROR: Cannot create directory: {}",
+        _output_path.clone().unwrap()
+    ));
+    let file = File::create(
+        format!(
+            "{path}{name}.md",
+            path = _output_path.clone().unwrap(),
+            name = distribution_config.comment
+        )
+        .as_str(),
+    )
+    .unwrap();
     let mut md = Markdown::new(file);
     let utc: DateTime<Utc> = Utc::now();
     md.write(format!("Cloudfront {}", distribution_config.comment).heading(1))
@@ -29,4 +43,17 @@ pub(crate) fn generate_markdown(distribution: Distribution) {
             .item(format!("web_acl_id: {}", distribution_config.web_acl_id.unwrap()).as_str()), //TODO: add more fields
     )
     .unwrap();
+}
+
+async fn distribution_data(distribution_id: &str) -> Distribution {
+    let creds = aws_config::load_from_env().await;
+    let client = aws_sdk_cloudfront::Client::new(&creds);
+    return client
+        .get_distribution()
+        .id(distribution_id)
+        .send()
+        .await
+        .unwrap()
+        .distribution
+        .unwrap();
 }
