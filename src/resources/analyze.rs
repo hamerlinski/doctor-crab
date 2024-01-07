778e2b6d5291742a197671d9a1aff0769be19919
Jakub Piotr Hamerliński <115890980+hamerlinski@users.noreply.github.com>
2024-01-07 17:24:24 +0100

    feat: move resource parsing to module

---
+++ src/resources/analyze.rs
@@ -0,0 +1,42 @@
+use crate::cli::arguments::Cli;
+use crate::resources::distro;
+use serde::Deserialize;
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Debug, Deserialize)]
+struct Resources {
+    cloudfront: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Env {
+    resources: Resources,
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    app: String,
+    #[serde(flatten)]
+    environments: std::collections::BTreeMap<String, Env>,
+}
+pub(crate) async fn interpret(path: String, arg: &Cli) {
+    let mut yaml_content = String::new();
+    File::open(path)
+        .expect("Could not find file")
+        .read_to_string(&mut yaml_content)
+        .expect("Could not read file");
+    let config: Config = serde_yaml::from_str(&yaml_content).expect("Could not open file.");
+    for (_env_name, env) in &config.environments {
+        distro::generate_markdown(
+            &env.resources.cloudfront,
+            Some(format!(
+                "{output_root}/{app}/{env}/",
+                output_root = arg.output_path.as_str(),
+                app = config.app,
+                env = _env_name
+            )),
+        )
+        .await;
+    }
+}
